5c4cf01d90542e19f45f9ff528b527f00b41f0bf
Liz Shulman <corgipower@hotmail.com>
2020-12-02 20:48:01 -0500

    mvp

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 let users = [
-	{ id: "1", name: "Jane Doe" },
-	{ id: "2", name: "John Doe" },
-	{ id: "3", name: "Jack Doe" },
+	{ id: "1", name: "Jane Doe", bio: 'My name is Jane'},
+	{ id: "2", name: "John Doe", bio: 'My name is John'},
+	{ id: "3", name: "Jack Doe", bio: 'My name is Jack'},
 ]
 
 function getUsers() {
